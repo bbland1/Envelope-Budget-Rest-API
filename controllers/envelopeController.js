b398d92038e101b3c94fde4cdfd4f489f8a4bfd3
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-12-20 02:09:36 -0500

    adding get by name and add envelope function

--- controllers/envelopeController.js
+++ controllers/envelopeController.js
@@ -1,5 +1,6 @@
 const envelopesDB = require('../db');
 
+
 //  TODO: add some response codes for letting user know what is going on
 
 // * function to get all the envelopes in the db
@@ -37,5 +38,55 @@ exports.getEnvelopesWithId = async (req, res) => {
     // send a something went wrong error
     //  ? should this be a different error code
     res.status(500).send(err);
+  }
+}
+
+exports.getEnvelopesWithName = async (req, res) => {
+  try {
+    // ! make sure that the params that is being used is specified in the req call, forgot it and spent way too long thinking something else was wrong
+    const envelopeName = req.params.name;
+
+    // get the db info and use the db function to filter the db for what is being looked for
+    const envelopes = await envelopesDB.envelopes;
+    const envelope = envelopesDB.findByName(envelopes, envelopeName);
+
+    // if the envelope doesn't exist send a not found error
+    if (!envelope) {
+      res.status(404).send({
+        message: `Envelope not found.`
+      })
+    }
+
+    // send the envelope
+    res.status(200).send(envelope);
+
+  } catch (err) {
+    // send a something went wrong error
+    //  ? should this be a different error code
+    res.status(500).send(err);
+  }
+}
+
+exports.addEnvelope = async (req, res) => {
+  try {
+    const { title, budget, saved } = req.body;
+    
+    const envelopes = await envelopesDB.envelopes;
+    // get a created id from the db
+    const newId = envelopesDB.newId(envelopes);
+
+    // pass all the new info
+    const newEnvelope = {
+      id: newId,
+      name: title,
+      // changes these to numbers from the strings that are passed
+      budget: parseInt(budget),
+      amountSaved: parseInt(saved),
+    }
+    
+    envelopes.push(newEnvelope);
+    res.status(201).send(envelopes);
+  } catch (err) {
+    res.status(500).send(err)
   }
 }
--- routes/envelopes.js
+++ routes/envelopes.js
@@ -2,11 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const envelopeController = require('../controllers/envelopeController');
+router.use(express.json());
+
+// !make sure to have this for parsing req.body it just is undefined without it
+router.use(express.urlencoded({ extended: false }));
 
 // * get all the envelopes
 router.get('/', envelopeController.getEnvelopes);
 
 // * get a specific envelope using the id parameter
 router.get('/:id', envelopeController.getEnvelopesWithId);
+
+// * to search for an envelope by name
+router.get('/envelope/:name', envelopeController.getEnvelopesWithName);
+
+// * add an envelope to the db
+router.post('/', envelopeController.addEnvelope);
 
 module.exports = router;
